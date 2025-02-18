0d5bda9362f133d24f1fdbcb9e35e77f583a3733
Gabriel <gabrielpachecotec@gmail.com>
2025-02-18 13:06:10 +0000

    Write retrievePossibleNextPositions function
    This function takes a point in an 8x8 grid chessboard(0-7) in the form of an
    array e.g. [x, y] and returns an array of the legal/possible
    nodes/next moves of a knight.

---
+++ knightsTravails.js
@@ -0,0 +1,24 @@
+function retrievePossibleNextPositions(arrPosition) {
+    let possiblePositions = [];
+    const p1 = [arrPosition[0] - 2, arrPosition[1] + 1];
+    const p2 = [arrPosition[0] - 1, arrPosition[1] + 2];
+    const p3 = [arrPosition[0] + 1, arrPosition[1] + 2];
+    const p4 = [arrPosition[0] + 2, arrPosition[1] + 1];
+    const p5 = [arrPosition[0] + 2, arrPosition[1] - 1];
+    const p6 = [arrPosition[0] + 1, arrPosition[1] - 2];
+    const p7 = [arrPosition[0] - 1, arrPosition[1] - 2];
+    const p8 = [arrPosition[0] - 2, arrPosition[1] - 1];
+
+    [p1, p2, p3, p4, p5, p6, p7, p8].forEach((position) => {
+        if (
+            position[0] >= 0 &&
+            position[0] <= 7 &&
+            position[1] >= 0 &&
+            position[1] <= 7
+        ) {
+            possiblePositions.push(position);
+        }
+    });
+
+    return possiblePositions;
+}
